d43c0625a3ccd6112154370fee8091832cdfa988
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2026-07-15 14:07:33 -0600

    fix: regmod runtime on embedded devices

--- command_registry.sh
+++ command_registry.sh
@@ -5,43 +5,6 @@ _PAD_SIZE=20
 
 alias register_module='core::obsolete register_module regmod; regmod'
 
-leg_regmod() {
-    core::hide_trace
-    local export_mod module_name cmds
-
-    [[ "$1" == export ]] && {
-        export_mod=true
-        shift
-    }
-
-    while [[ "$*" ]]; do
-        module_name="$1"
-
-        [[ ! "$module_name" ]] && {
-            core::error "No module names given"
-            return
-        }
-
-        cmds=()
-
-        while IFS= read -r cmd; do
-            [[ $cmd == $module_name* ]] && cmds+=("$cmd")
-            [[ "$export_mod" ]] && export -f "${cmd?}"
-        done < <(declare -F | cut -d ' ' -f 3 | grep -E ^"$module_name\.|$module_name::")
-
-        [[ "${#cmds[@]}" -eq 0 ]] && {
-            core::error "Failed to find any defined commands for module '$module_name'"
-            return
-        }
-
-        __REGISTERED_COMMANDS+=("${cmds[@]}")
-
-        core::verbose "[cyan]MODULE REGISTERED: ${module_name}[/]"
-
-        shift
-    done
-}
-
 regmod() {
     core::hide_trace
     local export_mod=false module_name fn match_found
